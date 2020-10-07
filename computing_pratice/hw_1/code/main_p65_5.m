43a6be4321a1e8ac2d2ed9c591b080325078d91d
jiahangok <2973493373@qq.com>
2020-10-07 17:38:12 +0800

    update.

--- computing_pratice/hw_1/code/main_p65_5.m
+++ computing_pratice/hw_1/code/main_p65_5.m
@@ -1,17 +1,21 @@
 disp("====(1)====")
+y=zeros(1,10)
 for n=1:10
     H=hilbert(n);
     disp(['n=' num2str(n)]);
     disp(['cond inf=' num2str(cond(H, inf))]);
+    y(1,n)=cond(H, inf);
 end
 
-disp("====(2)====")
-for n=1:10
+x = [1:1:5];
+plot(x, y(1,1:5),'rx-'), xlabel('n'), ylabel('cond'), legend('cond(H) inf')
+
+disp("====(2)&(3)====")
+for n=1:20
     disp(['n=' num2str(n)]);
     x=ones(n,1);
     H=hilbert(n);
     b=H*x;
-    b
     % Gauss消去法解方程组
     x_tilde=ColumnGussianElimination(H,b);
     % 计算剩余向量
@@ -22,6 +26,14 @@ for n=1:10
     r
     delta_x
     
+    % 分析有效数字位数
+    ineffective_num=0; % x_tilde向量中无效数字个数,无效数字就是有效数字位数是0
+    for j=1:n
+        if abs(delta_x(j,1))>0.5
+            ineffective_num=ineffective_num+1;
+        end
+    end
+    ineffective_num % x_tilde向量中无效数字个数
+    
 end
 
-disp("====(3)====")
